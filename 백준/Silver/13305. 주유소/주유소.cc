1a0c9e0cc1a95873d57b7737a97cd0fa64406de2
SoeYeong Kim <77740000+SoeYeong@users.noreply.github.com>
2025-08-01 05:07:15 +0900

    [Silver III] Title: 주유소, Time: 20 ms, Memory: 3584 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.cc
+++ 백준/Silver/13305. 주유소/주유소.cc
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int N, minCity;
 long long roadSum;
 long long city[100001], road[100001];
-long long cost[100001];
+long long cost;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
@@ -17,21 +18,15 @@ int main() {
         cin >> city[i];
     }
 
-    cost[0] = city[0] * road[0];
-    minCity = city[0];
-    for (int i = 1; i < N; i++) {
-        if (city[i] <= minCity) {
-            cost[i] = minCity * roadSum;
+    minCity = 1000000001;
+    for (int i = 0; i < N; i++) {
+        if (city[i] < minCity) {
             minCity = city[i];
-            roadSum = road[i];
         }
-        else {
-            roadSum += road[i];
-        }
-        cost[i] += cost[i - 1];
+        cost += (long long)minCity * road[i];
     }
 
-    cout << cost[N - 1];
+    cout << cost;
 
     return 0;
 }
