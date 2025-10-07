c520326b6e5df4c99b2defb9bfe1d72bf8a24b2b
SoeYeong Kim <77740000+SoeYeong@users.noreply.github.com>
2025-10-07 16:10:05 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 112 ms, Memory: 10228 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -1,29 +1,32 @@
-// DP
 #include <iostream>
-
 #define MAX 1025
 using namespace std;
 
 int N, M;
-long long arr[MAX][MAX];
-long long ans;
+int x_1, y_1, x_2, y_2;
+long long num[MAX][MAX];
+
 int main() {
     ios::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
+    cin.tie(NULL);
 
     cin >> N >> M;
     for (int i = 1; i <= N; i++) {
         for (int j = 1; j <= N; j++) {
-            cin >> arr[i][j];
-            arr[i][j] += arr[i - 1][j] + arr[i][j - 1] - arr[i - 1][j - 1];
+            cin >> num[i][j];
+            num[i][j] += num[i][j - 1];
+        }
+    }
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N; j++) {
+            num[i][j] += num[i - 1][j];
         }
     }
 
-    int x1, y1, x2, y2;
-    while (M--) {
-        cin >> x1 >> y1 >> x2 >> y2;
-        ans = arr[x2][y2] - arr[x1 - 1][y2] - arr[x2][y1 - 1] + arr[x1 - 1][y1 - 1];
-        cout << ans << '\n';
+    for (int i = 0; i < M; i++) {
+        cin >> x_1 >> y_1 >> x_2 >> y_2;
+        long long ans = num[x_2][y_2] - num[x_2][y_1 - 1] - num[x_1 - 1][y_2] + num[x_1 - 1][y_1 - 1];
+        cout << ans << "\n";
     }
 
     return 0;
